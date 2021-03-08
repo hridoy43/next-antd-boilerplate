f0ac8483fc2d3f53627e38dc35a7ce9d6a34b77a
hridoy43 <hridoy43@gmail.com>
2021-03-09 02:27:54 +0600

    antd theeming issue fixed

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const withAntdLess = require('next-plugin-antd-less');
 
 module.exports = withAntdLess({
-    // optional
-    modifyVars: {},
-    // optional
-    lessVarsFilePath: './styles/antd-variables.less',
-    // optional https://github.com/webpack-contrib/css-loader#object
-    cssLoaderOptions: {},
+    // //optional
+    // modifyVars: {},
+    // // optional
+    // lessVarsFilePath: './styles/antd-variables.less', //? doesn't need to specify the path if you already customize variable in global style.
+    // //optional https://github.com/webpack-contrib/css-loader#object
+    // cssLoaderOptions: {},
 
     // Other Config Here...
     webpack(config) {
